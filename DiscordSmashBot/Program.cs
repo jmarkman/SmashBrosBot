1ab03622e82aeeb1369ec2c6033bcc9e8714f8f8
Jonathan Markman <jmarkman446@gmail.com>
2019-10-29 00:46:24 -0400

    Add project files.

---
+++ DiscordSmashBot/Program.cs
@@ -0,0 +1,56 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DiscordSmashBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private static IConfigurationRoot _config;
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Configuration"))
+                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
+
+            _config = builder.Build();
+
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;
+
+            await _client.LoginAsync(TokenType.Bot, _config["ApiToken"]);
+            await _client.StartAsync();
+
+            // Block MainAsync from "completing" until the program is closed,
+            // i.e., the bot's existence is a task and once the task is done,
+            // the bot is subsequently deactivated
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+}
