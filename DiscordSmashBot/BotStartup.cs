bb9c333d23825a8edf1d35e36d7275cbca4ea46b
Jonathan Markman <jmarkman446@gmail.com>
2019-11-12 15:02:39 -0500

    complete extracting all setup logic from Progam and add comments and docstrings to BotStartup

--- DiscordSmashBot/BotStartup.cs
+++ DiscordSmashBot/BotStartup.cs
@@ -4,10 +4,7 @@ using Discord.WebSocket;
 using DiscordSmashBot.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordSmashBot
@@ -25,20 +22,35 @@ namespace DiscordSmashBot
             Configuration = cfgBuilder.Build();
         }
 
+        /// <summary>
+        /// Run the bot
+        /// </summary>
+        /// <param name="args">Arguments from the command line</param>
+        /// <returns></returns>
         public static async Task RunAsync(string[] args)
         {
             var botStart = new BotStartup(args);
             await botStart.RunAsync();
         }
 
+        /// <summary>
+        /// Gather the various services associated with the bot and use the startup service to run the bot
+        /// </summary>
+        /// <returns></returns>
         public async Task RunAsync()
         {
             var services = new ServiceCollection();
             ConfigureBotServices(services);
 
+            // Construct a provider from the services collection so that the various services can be used
             var provider = services.BuildServiceProvider();
+            // Start the logging service and command handling service; other services that aren't calling
+            // methods immediately after construction will follow these GetRequiredService calls
             provider.GetRequiredService<LoggingService>();
             provider.GetRequiredService<CommandHandlerService>();
+
+            // Start the bot: get API token, log in to bot account, begin websocket communication
+            await provider.GetRequiredService<BotStartupService>().StartBotAsync();
 
             // Block RunAsync from "completing" until the program is closed,
             // i.e., the bot's existence is a task and once the task is done,
@@ -46,6 +58,11 @@ namespace DiscordSmashBot
             await Task.Delay(-1);
         }
 
+        /// <summary>
+        /// Collects and performs rudimentary initialization for the various services
+        /// associated with the bot
+        /// </summary>
+        /// <param name="services"></param>
         private void ConfigureBotServices(IServiceCollection services)
         {
             services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
--- DiscordSmashBot/Program.cs
+++ DiscordSmashBot/Program.cs
@@ -9,23 +9,9 @@ namespace DiscordSmashBot
 {
     class Program
     {
-        private DiscordSocketClient _client;
-        private static IConfigurationRoot _config;
-
-        static void Main(string[] args)
+        public static Task Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Configuration"))
-                .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true);
-
-            _config = builder.Build();
-
-            new Program().MainAsync().GetAwaiter().GetResult();
-        }
-
-        public async Task MainAsync()
-        {
-            await Task.Delay(-1);
+            return BotStartup.RunAsync(args);
         }
     }
 }
