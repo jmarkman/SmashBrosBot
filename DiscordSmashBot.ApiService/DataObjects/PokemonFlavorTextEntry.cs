2803c099cd2cd1cd186e02395a3707a1514e9cf4
Jonathan Markman <jmarkman446@gmail.com>
2019-11-15 16:55:45 -0500

    create api interaction service project, start working with pokeapi

---
+++ DiscordSmashBot.ApiService/DataObjects/PokemonFlavorTextEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordSmashBot.ApiService.DataObjects
+{
+    public class PokemonFlavorTextEntry
+    {
+        /// <summary>
+        /// The name of the Pokemon
+        /// </summary>
+        public string PokemonName { get; set; }
+        /// <summary>
+        /// The associated Pokedex entry that Pokemon has
+        /// </summary>
+        public string PokedexEntry { get; set; }
+        /// <summary>
+        /// The game where said Pokedex entry came from
+        /// </summary>
+        public string CameFrom { get; set; }
+    }
+}
---
+++ DiscordSmashBot.ApiService/PokemonApiService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using DiscordSmashBot.ApiService.DataObjects;
+using PokeApiNet;
+using PokeApiNet.Models;
+
+namespace DiscordSmashBot.ApiService
+{
+    public class PokemonApiService
+    { 
+
+        public async Task<PokemonFlavorTextEntry> GetPokedexEntryAsyncFor(string pokemonName)
+        {
+            using (PokeApiClient pkmnClient = new PokeApiClient())
+            {
+                var pkmn = await pkmnClient.GetResourceAsync<Pokemon>(pokemonName);
+                var pkmnSpecies = await pkmnClient.GetResourceAsync(pkmn.Species);
+                var flavorTextEnglish = pkmnSpecies.FlavorTextEntries.Where(fte => fte.Language.Name == "en");
+
+                return new PokemonFlavorTextEntry
+                {
+                    PokemonName = pokemonName
+                };
+            }
+        }
+    }
+}
